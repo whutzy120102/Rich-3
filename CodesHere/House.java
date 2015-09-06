63382fffbdbb1248c08e3762d29c80f57f0c0a79
zhouwj <820537952@qq.com>
2015-09-07 02:34:23 +0800

    Update House.java
    更新了House.java类的部分内容

--- CodesHere/House.java
+++ CodesHere/House.java
@@ -3,23 +3,24 @@ package house;
 import java.awt.Color;
 
 /**
- *  House对象 有  房屋等级属性 、地点 属性，以及父类的 ，房屋所具有的颜色属性，获取土地单价等一些方法。
+ *  House对象 有  房屋等级属性，继承了地点类的    地区与地点 属性，以及父类的 获取土地单价方法。
  * @author Local_win8
  *
  */
 
 public class House{
-	private int houseRate;		// 房屋等级
+	private int houseRate;		// 房屋等级 0.1.2.3，在地图的面板上，用Z,Y,X,W分别对应表示
 	private String houseOwner;		// 房屋所有者名
 	private int houseOwnerState;	// 房主状态
 	private int location;
-	private Color houseColor;
+	
+	private int moneyFromHouse = 0;
+	
 	
 	public House(String owner, int houseRate, String district, int location) {
 		super();
 		this.houseOwner = owner;
 		this.houseRate = houseRate;
-//		super.district = district;
 		this.location = location;
 	}
 	
@@ -27,16 +28,13 @@ public class House{
 		
 	}
 	
-//	public House(String  owner, String fullLocation) {
-	public House(String  owner, int location, int state, Color color) {
+	public House(String  owner, int location, int state) {
 		this.houseRate = 0 ;
 		this.houseOwner = owner;
 		this.location = location;
 		this.houseOwnerState = state;
-		this.houseColor = color;
-//		this.fullLocation = fullLocation;
+		this.moneyFromHouse = 0;
 	}
-	
 	
 	
 	public int getLocation(){
@@ -94,9 +92,21 @@ public class House{
 	
 	// 当前房屋的应收取的过路费
 	public int getPassByCost() {
-		return getHouseMoney()/2;
+		if(houseOwnerState==1) {	// 正常状态
+			return getHouseMoney()/2;
+		}
+		return 0;		// 监狱或医院状态
 	}
-
+	
+	// 
+	public void setMoneyFromHouse(int money) {
+		this.moneyFromHouse = money;
+	}
+	// 房屋收取的过路费
+	public int getMoneyFromHouse() {
+		return moneyFromHouse;
+	}
+	
 	// 房屋的等级
 	public int getHouseRate() {
 		return houseRate;
@@ -106,18 +116,8 @@ public class House{
 		this.houseRate = houseRate;
 	}
 
-	public Color getHouseColor() {
-		return houseColor;
-	}
-
-	public void setHouseColor(Color houseColor) {
-		this.houseColor = houseColor;
-	}
-
 	public void setLocation(int location) {
 		this.location = location;
 	}
-	
-	
 	
 }
